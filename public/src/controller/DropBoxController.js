6d168745ede3c1bfa87fbf441773b0f6b46f3df8
Israel Cena <israelsjm@gmail.com>
2020-01-18 20:53:02 -0300

    create open file

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -1,3 +1,13 @@
 class DropBoxController {
-  constructor() {}
+  constructor() {
+    this.btnSendFileEl = document.querySelector('#btn-send-file');
+    this.inputFileEl = document.querySelector('#files');
+
+    this.initEvents();
+  }
+  initEvents() {
+    this.btnSendFileEl.addEventListener('click', (e) => {
+      this.inputFileEl.click();
+    });
+  }
 }
