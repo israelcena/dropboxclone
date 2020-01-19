268057c58f317a84ea456ee3d8d03af52795a449
Israel Cena <israelsjm@gmail.com>
2020-01-18 21:09:45 -0300

    Create 'SnackBar'

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -2,12 +2,18 @@ class DropBoxController {
   constructor() {
     this.btnSendFileEl = document.querySelector('#btn-send-file');
     this.inputFileEl = document.querySelector('#files');
+    this.snackModalEl = document.querySelector('#react-snackbar-root');
 
     this.initEvents();
   }
   initEvents() {
     this.btnSendFileEl.addEventListener('click', (e) => {
       this.inputFileEl.click();
+    });
+
+    this.inputFileEl.addEventListener('change', (e) => {
+      console.log(e.target.files);
+      this.snackModalEl.style.display = 'block';
     });
   }
 }
