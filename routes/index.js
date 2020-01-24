7855f5510fe3367b17ed2dccfb7449aab86c64e1
Israel Cena <israelsjm@gmail.com>
2020-01-23 21:30:51 -0300

    Create route for upload files

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+/* Upload Files */
+router.post('/upload', (req, res) => {
+  res.json(req.body);
 });
 
 module.exports = router;
