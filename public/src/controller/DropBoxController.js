86bc06680bc8404d239ddfdb8507854247bcf695
Israel Cena <israelsjm@gmail.com>
2020-01-23 21:24:40 -0300

    upLoadTask

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -6,14 +6,44 @@ class DropBoxController {
 
     this.initEvents();
   }
+
   initEvents() {
     this.btnSendFileEl.addEventListener('click', (e) => {
       this.inputFileEl.click();
     });
 
     this.inputFileEl.addEventListener('change', (e) => {
-      console.log(e.target.files);
+      this.upLoadTask(e.target.files);
+
       this.snackModalEl.style.display = 'block';
     });
+  }
+
+  upLoadTask(files) {
+    let promises = [];
+
+    [...files].forEach((file) => {
+      promises.push(
+        new Promise((resolve, reject) => {
+          let ajax = new XMLHttpRequest();
+
+          ajax.open('POST', '/upload');
+          ajax.onload = (e) => {
+            try {
+              resolve(JSON.parse(ajax.responseText));
+            } catch (e) {
+              reject(e);
+            }
+          };
+          ajax.onerror = (e) => {
+            reject(e);
+          };
+          let formData = new FormData();
+          formData.append('input-file', file);
+          ajax.send(formData);
+        })
+      );
+    });
+    return Promise.all(promises);
   }
 }
