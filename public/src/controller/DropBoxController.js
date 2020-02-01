58e969e75deabc504030f3ff30bcd6149662bbe6
Israel Cena <israelsjm@gmail.com>
2020-02-01 12:38:14 -0300

    add el in variables

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -2,7 +2,12 @@ class DropBoxController {
   constructor() {
     this.btnSendFileEl = document.querySelector('#btn-send-file');
     this.inputFileEl = document.querySelector('#files');
+
+    //SnackProgress
     this.snackModalEl = document.querySelector('#react-snackbar-root');
+    this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg');
+    this.timeleftEl = this.snackModalEl.querySelector('.timeleft');
+    this.filenameEl = this.snackModalEl.querySelector('.filename');
 
     this.initEvents();
   }
@@ -28,6 +33,7 @@ class DropBoxController {
           let ajax = new XMLHttpRequest();
 
           ajax.open('POST', '/upload');
+
           ajax.onload = (e) => {
             try {
               resolve(JSON.parse(ajax.responseText));
@@ -35,9 +41,15 @@ class DropBoxController {
               reject(e);
             }
           };
+
           ajax.onerror = (e) => {
             reject(e);
           };
+
+          ajax.upload.onprogress = (e) => {
+            this.uploadProgress(e, file);
+          };
+
           let formData = new FormData();
           formData.append('input-file', file);
           ajax.send(formData);
@@ -45,5 +57,13 @@ class DropBoxController {
       );
     });
     return Promise.all(promises);
+  }
+
+  uploadProgress(e, file) {
+    let loaded = e.loaded;
+    let total = e.total;
+    let porcent = parseInt((loaded / total) * 100);
+
+    this.progressBarEl.style.width = `${porcent}%`;
   }
 }
