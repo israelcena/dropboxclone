260c2250dad6b43b6fbeb34320f2b93d9f6f7200
israelcena <israelsjm@gmail.com>
2020-05-01 04:31:55 -0300

    reestyle code guide and include comments

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -1,108 +1,103 @@
 class DropBoxController {
-  constructor () {
-    this.btnSendFileEl = document.querySelector('#btn-send-file');
-    this.inputFileEl = document.querySelector('#files');
-    this.snackModalEl = document.querySelector('#react-snackbar-root');
-    this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg');
-    this.timeleftEl = this.snackModalEl.querySelector('.timeleft');
-    this.filenameEl = this.snackModalEl.querySelector('.filename');
-    this.initEvents();
-  }
+	constructor() {
+		this.btnSendFileEl = document.querySelector('#btn-send-file');
+		this.inputFileEl = document.querySelector('#files');
+		this.snackModalEl = document.querySelector('#react-snackbar-root');
+		this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg');
+		this.timeleftEl = this.snackModalEl.querySelector('.timeleft');
+		this.filenameEl = this.snackModalEl.querySelector('.filename');
+		this.initEvents();
+	}
 
-  initEvents() {
-    this.btnSendFileEl.addEventListener('click', (e) => {
-      this.inputFileEl.click();
-    });
+	initEvents() {
+		this.btnSendFileEl.addEventListener('click', (e) => {
+			this.inputFileEl.click();
+		});
 
-    this.inputFileEl.addEventListener('change', (e) => {
-      this.upLoadTask(e.target.files);
-      this.modalShow(true)
-      //reset input file
-      this.inputFileEl.value = ''
-    });
-  }
+		this.inputFileEl.addEventListener('change', (e) => {
+			this.upLoadTask(e.target.files);
+			this.modalShow(true);
+			//reset input file
+			this.inputFileEl.value = '';
+		});
+	}
 
-  modalShow(show = true) {
-    this.snackModalEl.style.display = (show) ? 'block' : 'none';
-  }
+	modalShow(show = true) {
+		this.snackModalEl.style.display = show ? 'block' : 'none';
+	}
 
-  upLoadTask(files) {
-    let promises = [];
+	upLoadTask(files) {
+		let promises = [];
 
-    [...files].forEach((file) => {
-      promises.push(
-        new Promise((resolve, reject) => {
-          let ajax = new XMLHttpRequest();
+		[...files].forEach((file) => {
+			promises.push(
+				new Promise((resolve, reject) => {
+					let ajax = new XMLHttpRequest();
 
-          ajax.open('POST', '/upload');
+					ajax.open('POST', '/upload');
 
-          ajax.onload = (event) => {
-            this.modalShow(false)
-            try {
-              resolve(JSON.parse(ajax.responseText));
-            } catch (event) {
-              reject(event);
-            }
-          };
+					ajax.onload = (event) => {
+						this.modalShow(false);
+						try {
+							resolve(JSON.parse(ajax.responseText));
+						} catch (event) {
+							reject(event);
+						}
+					};
 
-          ajax.onerror = (event) => {
-            this.modalShow(false)
-            reject(event);
-          };
+					ajax.onerror = (event) => {
+						this.modalShow(false);
+						reject(event);
+					};
 
-          ajax.upload.onprogress = (event) => {
-            this.uploadProgress(event, file);
-          };
+					ajax.upload.onprogress = (event) => {
+						this.uploadProgress(event, file);
+					};
 
-          let formData = new FormData();
-          formData.append('input-file', file);
-          // Count time on init upload file
-          this.startUploadTime = Date.now();
+					let formData = new FormData();
+					formData.append('input-file', file);
+					// Count time on init upload file
+					this.startUploadTime = Date.now();
+					ajax.send(formData);
+				})
+			);
+		});
+		return Promise.all(promises);
+	}
 
-          ajax.send(formData);
-        })
-      );
-    });
-    return Promise.all(promises);
-  }
+	uploadProgress(event, file) {
+		// Count time spent for upload file
+		let timeSpent = Date.now() - this.startUploadTime;
+		let loaded = event.loaded;
+		let total = event.total;
+		let porcent = parseInt((loaded / total) * 100);
+		let timeLeft = ((100 - porcent) * timeSpent) / porcent;
+		this.progressBarEl.style.width = `${porcent}%`;
+		this.filenameEl.innerHTML = file.name;
+		this.timeleftEl.innerHTML = this.FormatTime(timeLeft);
+	}
 
-  uploadProgress(event, file) {
-    // Count time spent for upload file
-    let timeSpent = Date.now() - this.startUploadTime;
+	FormatTime(duration) {
+		let seconds = parseInt((duration / 1000) % 60);
+		let minutes = parseInt((duration / (1000 * 60)) % 60);
+		let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
 
-    let loaded = event.loaded;
-    let total = event.total;
-    let porcent = parseInt((loaded / total) * 100);
-    let timeLeft = ((100 - porcent) * timeSpent) / porcent
-    this.progressBarEl.style.width = `${porcent}%`;
+		if (duration === Infinity) {
+			return `Calculando tempo estimado`;
+		}
 
-    this.filenameEl.innerHTML = file.name;
-    this.timeleftEl.innerHTML = this.FormatTime(timeLeft);
+		if (hours > 0) {
+			return `${hours} horas, ${minutes} minutos e ${seconds} segundos restantes`;
+		}
 
-    console.log(timeSpent, timeLeft, porcent);
-  }
+		if (minutes > 0) {
+			return `${minutes} minutos e ${seconds} segundos restantes`;
+		}
 
-  FormatTime(duration) {
-    let seconds = parseInt((duration / 1000) % 60)
-    let minutes = parseInt((duration / (1000 * 60)) % 60)
-    let hours = parseInt((duration / (1000 * 60 * 60)) % 24)
+		if (seconds > 0) {
+			return `${seconds} segundos restantes`;
+		}
 
-    if (duration === Infinity) {
-      return `Calculando tempo estimado`
-    }
-
-    if (hours > 0) {
-      return `${hours} horas, ${minutes} minutos e ${seconds} segundos restantes`
-    }
-
-    if (minutes > 0) {
-      return `${minutes} minutos e ${seconds} segundos restantes`
-    }
-
-    if (seconds > 0) {
-      return `${seconds} segundos restantes`
-    }
-
-    return `Concluido`
-  }
+		return `Concluido`;
+	}
 }
