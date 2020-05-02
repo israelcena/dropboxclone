7e3c76c0753c3da47d6f8bc11ed8f8b9eff3d1ff
israelcena <israelsjm@gmail.com>
2020-05-01 23:38:06 -0300

    include delete route

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,47 @@
-var express = require('express');
-var router = express.Router();
-var formidable = require('formidable');
+var express = require('express')
+var router = express.Router()
+var formidable = require('formidable')
+var fs = require('fs')
 
-/* GET home page. */
 router.get('/', (req, res, next) => {
-	res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Express' })
+})
 
-/* Upload Files */
+router.delete('/file', (req, res) => {
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true
+  })
+
+  form.parse(req, (err, fields, files) => {
+    let path = './' + fields.path
+    if (fs.existsSync(path)) {
+      fs.unlink(path, (err) => {
+        if (err) {
+          res.status(400).json({
+            err
+          })
+        } else {
+          res.json({
+            fields
+          })
+        }
+      })
+    }
+  })
+})
+
 router.post('/upload', (req, res) => {
-	let form = new formidable.IncomingForm({
-		uploadDir: './upload',
-		keepExtensions: true,
-	});
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true
+  })
 
-	form.parse(req, (err, fields, files) => {
-		res.json({
-			files,
-		});
-	});
-});
+  form.parse(req, (err, fields, files) => {
+    res.json({
+      files
+    })
+  })
+})
 
-module.exports = router;
+module.exports = router
