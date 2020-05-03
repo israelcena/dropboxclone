825049fb2391a4d27a9b659f7a6979531a55b209
israelcena <israelsjm@gmail.com>
2020-05-03 11:21:55 -0300

    read files and download

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,27 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' })
 })
 
+router.get('/file', (req, res) => {
+  let path = './' + req.query.path
+
+  if (fs.existsSync(path)) {
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        console.error(err)
+        res.status(400).json({
+          error: err
+        })
+      } else {
+        res.status(200).end(data)
+      }
+    })
+  } else {
+    res.status(404).json({
+      error: 'File not found.'
+    })
+  }
+})
+
 router.delete('/file', (req, res) => {
   let form = new formidable.IncomingForm({
     uploadDir: './upload',
