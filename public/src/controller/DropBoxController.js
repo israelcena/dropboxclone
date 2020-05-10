c82cb6e5f4d55978b1e8943cba694cd35c9dd64f
israelcena <israelsjm@gmail.com>
2020-05-10 02:01:29 -0300

    include files in firebase

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -30,12 +30,18 @@ class DropBoxController {
     this.getSelection().forEach((li) => {
       let file = JSON.parse(li.dataset.file)
       let key = li.dataset.key
-      let formData = new FormData()
 
-      formData.append('path', file.path)
-      formData.append('key', key)
+      promises.push(new Promise((resolve, reject) => {
+        let fileRef = firebase.storage().ref(this.currentFolder.join('/')).child(file.name)
 
-      promises.push(this.ajax('/file', 'DELETE', formData))
+        fileRef.delete().then(() => {
+          resolve({
+            fields: { key }
+          })
+        }).catch(err => {
+          reject(err)
+        })
+      }))
     })
     return Promise.all(promises)
   }
